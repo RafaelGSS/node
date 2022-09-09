202a8237de6afa3cdeb3e1798da1354b114c9316
RafaelGSS <rafael.nunu@hotmail.com>
2022-09-09 16:38:08 -0300

    sync

--- src/req_wrap-inl.h
+++ src/req_wrap-inl.h
@@ -157,6 +157,7 @@ int ReqWrap<T>::Dispatch(LibuvFunction fn, Args... args) {
       env()->event_loop(),
       req(),
       MakeLibuvRequestCallback<T, Args>::For(this, args)...);
+  std::cout << "Err: " << err << std::endl;
   if (err >= 0) {
     ClearWeak();
     env()->IncreaseWaitingRequestCounter();
