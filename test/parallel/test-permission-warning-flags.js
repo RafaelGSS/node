51ed1155b9c1b903d5cf044b297393877b1115a2
RafaelGSS <rafael.nunu@hotmail.com>
2022-12-26 17:24:14 -0300

    lib: include warning to -allow-child and -allow-worker

---
+++ test/parallel/test-permission-warning-flags.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const { spawnSync } = require('child_process');
+const assert = require('assert');
+
+const warnFlags = [
+  '--allow-child-process',
+  '--allow-worker',
+];
+
+for (const flag of warnFlags) {
+  const { status, stderr } = spawnSync(
+    process.execPath,
+    [
+      '--experimental-permission', flag, '-e',
+      'setTimeout(() => {}, 1)',
+    ]
+  );
+
+  const flagWarn = stderr.toString().split('\n')[2];
+  assert.match(flagWarn, new RegExp(`SecurityWarning: The flag ${flag} must be used under extremely cautious`));
+  assert.strictEqual(status, 0);
+}
