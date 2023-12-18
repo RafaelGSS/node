b417898adc3e0ae29a43d858c0b94d87363e9e87
RafaelGSS <rafael.nunu@hotmail.com>
2023-12-18 17:17:44 -0300

    permission: fix wildcard when children > 1

--- test/parallel/test-permission-fs-wildcard.js
+++ test/parallel/test-permission-fs-wildcard.js
@@ -98,3 +98,21 @@ if (common.isWindows) {
   );
   assert.strictEqual(status, 0, stderr.toString());
 }
+
+{
+  const { status, stderr } = spawnSync(
+    process.execPath,
+    [
+      '--experimental-permission',
+      '--allow-fs-read=/a/b/*',
+      '--allow-fs-read=/a/b/d',
+      '-e',
+      `
+        const assert = require('assert')
+        assert.ok(process.permission.has('fs.read', '/a/b/c'));
+        assert.ok(!process.permission.has('fs.read', '/a/c/c'));
+      `,
+    ]
+  );
+  assert.strictEqual(status, 0, stderr.toString());
+}
